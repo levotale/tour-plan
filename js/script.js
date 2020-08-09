d47d1189e3348b279758418c7b7157e4a317b48d
votale <levotale@gmail.com>
2020-08-09 05:48:25 +0300

    create: validation for forms

--- js/script.js
+++ js/script.js
@@ -73,4 +73,21 @@ $(document).ready(function () {
     modalOverlay.removeClass("modal__overlay_active");
     modalDialog.removeClass("modal__dialog_active");
   }
+  $(".form").each(function () {
+    $(this).validate({
+      errorClass: "invalid",
+      messages: {
+        name: {
+          required: "Please specify your name",
+        },
+        email: {
+          required: "We need your email address to contact you",
+          email: "Your email address must be in the format of name@domain.com",
+        },
+        phone: {
+          required: "Please input phone number",
+        },
+      },
+    });
+  });
 });
